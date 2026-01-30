39b470575b70a2dc199ffe5a1e8eb2798d7f9f27
minhtiendo067 <minhtiendo067@gmail.com>
2026-01-30 04:52:08 -0800

    Create World.java

---
+++ World.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // This is the entry point of the application
+        System.out.println("Hello, World!"); // Prints the message
+    }
+}
